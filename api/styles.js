73d9627fc187dfac75f188b9904d9611ccd9e691
Carlos yaco <cyaco33@gmail.com>
2021-04-06 11:39:42 -0500

    first test pdf make

---
+++ api/styles.js
@@ -0,0 +1,11 @@
+module.exports = {
+    header :{
+        bold:true,
+        fontsize:12,
+        aligment:"center",
+        color:"#000000",
+    },
+    label:{
+        color:"#ff0000",
+    }
+}
